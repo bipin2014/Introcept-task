033bdc2ac22df45cbadd805f2f5e46a317f7c268
Bipin Budhathoki <bipinbudhathoki2014@gmail.com>
2021-03-11 11:27:23 +0545

    Removed default readme, create client model and clientcontroller,added routes in api.php and remove default routes

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Client;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class ClientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $path=$this->check_file_exists_and_return_path();
+        $file = fopen($path, "r");
+        $all_data = array();
+        while ( ($data = fgetcsv($file, 200, ",")) !==FALSE ) {
+            $client=new Client();
+            array_push($all_data, $client->array_to_object_array($data));
+        }
+        return response()->json([
+            'data'=>$all_data
+        ]);
+    }
+
+    function check_file_exists_and_return_path(){
+        $file_name='clients_data.csv';
+        $exists = Storage::disk('local')->exists($file_name);
+        $path=Storage::path($file_name);
+        //If file not exists create one
+        if (!$exists) {
+            $file = fopen($path, 'w');
+            $column=array('Name', 'Gender', 'Phone', 'Email', 'Address','Nationality','Dob','Education Background','Contact Mode');
+             // save the column headers
+            fputcsv($file, $column);
+            fclose($file);
+        }
+        return $path;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //Validate data
+
+        $validated = $request->validate([
+            'name' => 'required|min:3|max:50',
+            'gender' => 'required',
+            'phone' => 'required|digits:10',
+            'email' => 'required|email',
+            'address' => 'required|min:5',
+            'nationality' => 'required',
+            'dob' => 'required|date',
+            'education_background' => 'required',
+            'contact_mode' => 'required',
+        ]);
+        //data is valid 
+        $path=$this->check_file_exists_and_return_path();
+        //Store data to csv
+        $client=new Client();
+        $dataarray=$client->array_object_to_array($request);
+        $this->append_to_file($path,$dataarray);
+        return response()->json([
+            'message' => 'Data Added Sucessfullt to csv file',
+        ]);
+    }
+
+    private function append_to_file($path,$dataarray){
+        $file = fopen($path, 'a');
+        fputcsv($file, $dataarray);
+        fclose($file);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Client  $client
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Client $client)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Client  $client
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Client $client)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Client  $client
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Client $client)
+    {
+        //
+    }
+}
---
+++ app/Models/Client.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    use HasFactory;
+
+    public function array_to_object_array($arrayData){
+    return [
+        'name' => $arrayData[0],
+        'gender' => $arrayData[1],
+        'phone' => $arrayData[2],
+        'email' => $arrayData[3],
+        'address' => $arrayData[4],
+        'nationality' => $arrayData[5],
+        'dob' => $arrayData[6],
+        'education_background' => $arrayData[7],
+        'contact_mode' => $arrayData[8],
+        ];
+    }
+    public function array_object_to_array($objectData){
+        return [
+            $objectData->name,
+            $objectData->gender,
+            $objectData->phone,
+            $objectData->email,
+            $objectData->address,
+            $objectData->nationality,
+            $objectData->dob,
+            $objectData->education_background,
+            $objectData->contact_mode,
+            ];
+        }
+}
